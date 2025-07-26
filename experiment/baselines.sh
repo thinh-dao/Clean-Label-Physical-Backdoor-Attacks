0a7aadcdff3a02d410252c00bc7fdedcb368ee74
21thinh <21thinh.dd@vinuni.edu.vn>
2025-07-26 12:07:51 +0700

    update baseline experiment

--- experiment/baselines.sh
+++ experiment/baselines.sh
@@ -14,6 +14,11 @@ python main.py --recipe=dirty-label --scenario=transfer --net=deit_tiny --optimi
 python main.py --recipe=dirty-label --scenario=finetuning --net=deit_tiny --optimization=transformer-adamw --dataset=Animal_classification --trigger=tennis --poisonkey=11-28 --alpha=0.1 --devices=0,1 --model_seed=123456 --poison_seed=123456 --exp_name=dirty_label &
 python main.py --recipe=dirty-label --scenario=transfer --net=deit_tiny --optimization=transformer-adamw --dataset=Animal_classification --trigger=tennis --poisonkey=11-28 --alpha=0.1 --devices=1,0 --model_seed=123456 --poison_seed=123456 --exp_name=dirty_label &
 
+python main.py --recipe=dirty-label --scenario=finetuning --net=deit_tiny --optimization=transformer-adamw --augment --paugment --data_aug=mixed --model_seed=1234567 --dataset=Animal_classification --trigger=tennis --poisonkey=11-19 --alpha=0.1 --devices=3,4 --poison_seed=123456 --exp_name=dirty-label & \
+python main.py --recipe=dirty-label --scenario=transfer --net=deit_tiny --optimization=transformer-adamw --augment --paugment --data_aug=mixed --model_seed=1234567 --dataset=Animal_classification --trigger=tennis --poisonkey=11-19 --alpha=0.1 --devices=4,3 --poison_seed=123456 --exp_name=dirty-label & \
+python main.py --recipe=dirty-label --scenario=finetuning --net=deit_tiny --optimization=transformer-adamw --augment --paugment --data_aug=mixed --model_seed=1234567 --dataset=Animal_classification --trigger=tennis --poisonkey=11-28 --alpha=0.1 --devices=4,3 --poison_seed=123456 --exp_name=dirty-label & \
+python main.py --recipe=dirty-label --scenario=transfer --net=deit_tiny --optimization=transformer-adamw --augment --paugment --data_aug=mixed --model_seed=1234567 --dataset=Animal_classification --trigger=tennis --poisonkey=11-28 --alpha=0.1 --devices=4,3 --poison_seed=123456 --exp_name=dirty-label &
+
 ######### Naive Attack #########
 python main.py --recipe=naive --scenario=finetuning --dataset=Facial_recognition_extended --trigger=sunglasses --poisonkey=9-5 --alpha=0.1 --devices=3,4 --model_seed=123456 --poison_seed=123456 --exp_name=naive & \
 python main.py --recipe=naive --scenario=transfer --dataset=Facial_recognition_extended --trigger=sunglasses --poisonkey=9-5 --alpha=0.1 --devices=3,4 --model_seed=123456 --poison_seed=123456 --exp_name=naive & \
@@ -54,3 +59,6 @@ python main.py --recipe=label-consistent --scenario=transfer --net=resnet18_imag
 
 python main.py --recipe=label-consistent --scenario=finetuning --net=deit_tiny --optimization=transformer-adamw --dataset=Animal_classification --trigger=tennis --attackiter=250 --poisonkey=11-19 --alpha=0.1 --devices=2,3 --model_seed=123456 --poison_seed=123456 --exp_name=label_consistent & 
 python main.py --recipe=label-consistent --scenario=finetuning --net=deit_tiny --optimization=transformer-adamw --dataset=Animal_classification --trigger=phone --attackiter=250 --poisonkey=11-28 --alpha=0.1 --devices=3,2 --model_seed=123456 --poison_seed=123456 --exp_name=label_consistent & 
+
+python main.py --recipe=label-consistent --scenario=finetuning --net=deit_tiny --optimization=transformer-adamw --augment --paugment --data_aug=mixed --model_seed=1234567 --dataset=Animal_classification --trigger=tennis --poisonkey=11-19 --alpha=0.1 --devices=0,1 --poison_seed=123456 --exp_name=label-consistent & \
+python main.py --recipe=label-consistent --scenario=transfer --net=deit_tiny --optimization=transformer-adamw --augment --paugment --data_aug=mixed --model_seed=1234567 --dataset=Animal_classification --trigger=tennis --poisonkey=11-19 --alpha=0.1 --devices=1,0 --poison_seed=123456 --exp_name=label-consistent & 
