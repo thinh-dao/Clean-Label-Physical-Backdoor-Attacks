897c25faa24a9ab0ac61d423cde6d43606a200d8
thinh-dao <dthinh.forwork@gmail.com>
2025-07-31 06:57:55 +0000

    minor update to exp setting

--- experiment/transferability.sh
+++ experiment/transferability.sh
@@ -27,14 +27,14 @@ python main.py --devices=4,3 --recipe=gradient-matching --source_criterion=cw \
 # Resnet34
 python main.py --devices=3,4 --recipe=gradient-matching --source_criterion=cw \
     --attackiter=3000 --scenario=finetuning --retrain_scenario=from-scratch --retrain_iter=750 \
-    --net=resnet34_imagenet --ensemble=4 --vnet=resnet18_imagenet,vgg11_imagenet,mobilenetv2_imagenet,deit_tiny --vruns=1 --dataset=Animal_classification --poisonkey=11-19 --trigger=tennis \
+    --net=resnet34_imagenet --vnet=resnet18_imagenet,vgg11_imagenet,mobilenetv2_imagenet,deit_tiny --vruns=1 --dataset=Animal_classification --poisonkey=11-19 --trigger=tennis \
     --alpha=0.2 --eps=16 --alpha=0.2 --model_seed=123456 --poison_seed=123456 \
     --save_poison=poison_only --exp_name=transfer_models & \
 
 # MobileNetV3
-python main.py --devices=3,4 --recipe=gradient-matching --source_criterion=cw \
+python main.py --devices=0,1,4 --recipe=gradient-matching --source_criterion=cw \
     --attackiter=3000 --scenario=finetuning --retrain_scenario=from-scratch --retrain_iter=750 \
-    --net=mobilenetv3_imagenet --ensemble=4 --vnet=resnet18_imagenet,vgg11_imagenet,mobilenetv2_imagenet,deit_tiny --vruns=1 --dataset=Animal_classification --poisonkey=11-19 --trigger=tennis \
+    --net=mobilenetv3_imagenet --vnet=resnet18_imagenet,vgg11_imagenet,mobilenetv2_imagenet,deit_tiny --vruns=1 --dataset=Animal_classification --poisonkey=11-19 --trigger=tennis \
     --alpha=0.2 --eps=16 --alpha=0.2 --model_seed=123456 --poison_seed=123456 \
     --save_poison=poison_only --exp_name=transfer_models & \
 
